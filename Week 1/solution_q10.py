a5e8066df01fed0b074796de489e480e3d3aaeb4
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2023-06-15 15:51:26 +0700

    finalized the finalized

--- Week 1/solution_q10.py
+++ Week 1/solution_q10.py
@@ -8,7 +8,7 @@ for i in range(n):
 
 def kadane_algorithm(A):
     max_global = max_current = 0
-    for i in range(1, len(A)):
+    for i in range(0, len(A)):
         max_current = max(A[i], max_current + A[i])
         max_global = max(max_global, max_current)
     return max_global
