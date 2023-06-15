1966af718d087cf778b8e3cc8ae9c48d464cf95c
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2023-06-15 14:09:46 +0700

    init & solutions for questions 1-2

---
+++ Week 1/solution_q2.py
@@ -0,0 +1,27 @@
+import time 
+start = time.process_time()
+
+li = list(map(int, input().split()))
+
+def Sum(x,i,j):
+    s = 0
+    for k in range(i,j+1):
+        s +=  x[k]
+    return(s)
+
+def find_max_subsequence_sum(li):
+    n = len(li)
+    max_sum = float('-inf')
+
+    for i in range(n):
+        for j in range(i, n):
+            current_sum = Sum(li, i, j)
+            max_sum = max(max_sum, current_sum)
+
+    return max_sum
+
+result = find_max_subsequence_sum(li)
+print("The sum of the maximum subsequence is:", result)
+
+finish = time.process_time()
+print("Running time: ", finish-start, " seconds")
