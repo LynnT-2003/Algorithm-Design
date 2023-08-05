15c68257fbbd80777df0646f6b1e62dbb88ec2e2
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2023-08-06 05:28:31 +0700

    Completed Question 6 from a Midterm Sample Paper (Kadane's Algorithm).

---
+++ za_Midterms_1/Question 1/q1.py
@@ -0,0 +1,19 @@
+predicted_changes  = list(map(int, input().split()))
+
+def max_profit(predicted_changes):
+    n = len(predicted_changes)
+
+    # Initialize variables to keep track of the maximum profit and current profit
+    max_profit = predicted_changes[0]
+    current_profit = predicted_changes[0]
+
+    # Loop through the predicted changes to find the maximum sum subarray
+    for i in range(1, n):
+        current_profit = max(predicted_changes[i], current_profit + predicted_changes[i])
+        max_profit = max(max_profit, current_profit)
+
+    return max_profit
+
+# Example usage:
+print(max_profit(predicted_changes))  # Output: 12
+
