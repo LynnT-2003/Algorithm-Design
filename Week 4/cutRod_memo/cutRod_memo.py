5d692004854eb6af1a3328014a82c44393bb7d5f
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2023-08-08 03:37:56 +0700

    Finalized cutRod with Dynamic Programming.

--- Week 4/cutRod_memo/cutRod_memo.py
+++ Week 4/cutRod_memo/cutRod_memo.py
@@ -9,15 +9,13 @@ memo = [None] * (n+1)
 maximumRevenue = float('-inf')
 
 def maxRev(l):
-    global total
-    global counter
-    global calls
-    global memo
-    global maximumRevenue
-
+    global total, counter, calls, memo, maximumRevenue
+    
+    # Check memoization
     if memo[l] is not None:
         return memo[l]
 
+    # Main Logic
     if l == 0:
         return 0
     
@@ -25,11 +23,19 @@ def maxRev(l):
         remaining = l - i
         total = prices[i-1] + maxRev(remaining)
         maximumRevenue = max(maximumRevenue, total)
+
+    # Record memoization
     memo[l] = maximumRevenue
     
     counter += 1    
     calls[l] += 1
     return maximumRevenue
 
+# Reverse because the base case is the smallest
+for i in range(n, -1, -1):
+    maxRev(i)
+# Print out the last index of memo since we want the maximum revenue
+print(memo[-1])
+
 print(f"Maximum Revenue: {maxRev(n)}, {counter} calls")
-print("Number of calls for each maxRev():", calls[1:])
+# print("Number of calls for each maxRev():", calls[1:])
