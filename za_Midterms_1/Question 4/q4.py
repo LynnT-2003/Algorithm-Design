7261c7752f914c55a3d39ff2228f0f43c7f7c469
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2023-08-08 01:56:53 +0700

    Refactored Question 4.

--- za_Midterms_1/Question 4/q4.py
+++ za_Midterms_1/Question 4/q4.py
@@ -1,17 +1,11 @@
-def get_num_of_aliens(n):
-    if n <= 0:
-        return "Invalid input. The position should be a positive integer."
-    elif n == 1:
-        return 0
-    elif n == 2:
-        return 1
-    else:
-        fib_sequence = [0, 1]
-        for i in range(2, n):
-            next_number = fib_sequence[-1] + fib_sequence[-2]
-            fib_sequence.append(next_number)
-        return fib_sequence[-1]
+# Fibonacci Sequence
 
-gen = int(input())
-fibonacci_number = get_num_of_aliens(gen + 3)
-print(f"Answer: {fibonacci_number}")
+def fibonacci(n) :  
+    result = [1,1]
+    for i in range(len(result), n+2):
+        num = result[i-1] + result[i-2]
+        result.append(num)
+    print(result[-1])
+
+n = int(input())
+fibonacci(n)
