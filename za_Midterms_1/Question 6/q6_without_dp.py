bf59be30bce119e87ab4be1409f09dc1129abff9
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2023-08-08 02:46:03 +0700

    BalanceSplit without DP for easier understanding.

---
+++ za_Midterms_1/Question 6/q6_without_dp.py
@@ -0,0 +1,34 @@
+import sys
+
+data = list(map(int, input().split()))
+N = len(data)
+
+x = [None for _ in range(N)]
+min_diff = sys.maxsize
+total_sum = sum(data)
+
+def Combination(i, group1_sum):
+    
+    global min_diff, x, N
+
+    if i == N:
+        group2_sum = total_sum - group1_sum
+        diff = abs(group1_sum - group2_sum)
+
+        if diff < min_diff:
+            min_diff = diff
+            
+        return 1
+    else:
+        # Add items from group 1
+        v = Combination(i+1, group1_sum + data[i])
+        # Dont't add items from group 1
+        # Pay attention to: "group2_sum = total_sum - group1_sum"
+        # Therefore, when the stop condition is reached 
+        # the items from group2 are accordingly added to group2_sum 
+        v += Combination(i+1, group1_sum) 
+        return v
+
+Combination(0, 0)
+
+print(min_diff)
