475ed3226384c417c2a25c9e2b25e21f057f5578
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2023-10-09 06:21:29 +0700

    Updated clarity for BoundKnapSack

--- Finals Practice/2-2021/Question 7/q7.py
+++ Finals Practice/2-2021/Question 7/q7.py
@@ -29,13 +29,13 @@ def Bound(i, sumW, sumV):
     # M-sumW-RightWeight > 0 means there is still space in the knapsack
 
     # t < N means there are still items to be considered
-    while j < N and propotion == 1:
-        # if the item[t] can be put into the knapsack physically then propotion is 1
-        # otherwise propotion is the proportion of the item[t] that can be put into the knapsack
+    while j < N and proportion == 1:
+        # if the item[t] can be put into the knapsack physically then proportion is 1
+        # otherwise proportion is the proportion of the item[t] that can be put into the knapsack
 
-        propotion = min(M - sumW - RightWeight, item[j].w) / item[j].w
-        RightWeight += propotion * item[j].w
-        SV += propotion * item[j].v
+        proportion = min(M - sumW - RightWeight, item[j].w) / item[j].w
+        RightWeight += proportion * item[j].w
+        SV += proportion * item[j].v
         j += 1
 
     return SV + sumV
@@ -50,7 +50,8 @@ def dfs(i, sumW, sumV):
     else:
         # only consider if it is possible to put item[h] into the knapsack
         if sumW + item[i].w <= M:
-            dfs(i+1, sumW+item[i].w, sumV+item[i].v)
+            newsumW = sumW + item[i].w
+            dfs(i+1, newsumW, sumV+item[i].v)
 
         # Bound is the maximum possible value of the knapsack illegally
         # If you cheat and still cannot get a better solution, then there is no need to consider the rest of the items
@@ -61,3 +62,22 @@ def dfs(i, sumW, sumV):
 item.sort(key=lambda x: x.r, reverse=True)
 dfs(0, 0, 0)
 print(maxV)
+
+# Explanation:
+# - The `dfs` function takes three arguments:
+#   - `i` represents the index of the current item.
+#   - `sumW` is the current total weight in the knapsack.
+#   - `sumV` is the current total value in the knapsack.
+# - `count` keeps track of the number of recursive calls.
+# - `maxBound` stores the maximum achievable value estimated by the Bound function.
+
+# Within the function:
+# - It first calculates the upper bound (`maxBound`) based on the current state.
+# - Then, it checks if we have reached the end of the item list (`i == N`).
+#   - If so, it updates `maxV` with the maximum value found so far.
+# - If we haven't reached the end of the item list:
+#   - It explores two branches:
+#     1. Takes the current item (if it fits within the knapsack's capacity) and recursively calls `dfs` with updated parameters.
+#     2. Skips the current item if its inclusion doesn't exceed the upper bound, avoiding unnecessary exploration.
+
+# This function uses pruning based on the upper bound (`maxBound`) to optimize the search, only exploring branches that have the potential to lead to better solutions, improving the efficiency of the Knapsack problem-solving algorithm.
