1b450d1d210777afa56daacba33f2f8f2935f4d3
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2023-10-09 06:46:34 +0700

    Updated print statements for better clarity.

--- Finals Practice/2-2021/Question 5/q5.py
+++ Finals Practice/2-2021/Question 5/q5.py
@@ -40,14 +40,27 @@ def BFS(s):
 
     while Q:
         node = Q.pop(0)
+        print(f"This is how it looks now (Current State):")
+        for i in range(0, 6, 3):
+            print(node.board[i:i+3])  # Display the grid in a 3x2 format
+        print()
+
         if is_goal(node):
+            print("Goal state reached!")
             return node.g
 
         for suc in successor(node):
             if tuple(suc.board) not in Reached:
                 Reached.add(tuple(suc.board))
                 Q.append(suc)
+                print(f"Trying a Move (Adding State to Queue):")
+                for i in range(0, 6, 3):
+                    print(suc.board[i:i+3])  # Display the grid in a 3x2 format
+                print()
     return False
 
-
-print(BFS(state(board)))
+result = BFS(state(board))
+if result:
+    print(f"Minimum number of swaps to reach the goal: {result}")
+else:
+    print("Goal state cannot be reached.")
